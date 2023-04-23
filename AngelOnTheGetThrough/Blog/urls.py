68f57041b9cd7345920ab78c5819f6d8b4f434e5
BertinAm <bertinfonge0@gmail.com>
2023-04-23 06:34:12 +0100

    updates

--- AngelOnTheGetThrough/Blog/urls.py
+++ AngelOnTheGetThrough/Blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# creating the first path with route set empty strings , with view set to views.home and name set to Blog-home
 urlpatterns = [
     path('', views.home, name='Blog-home')
 ]
